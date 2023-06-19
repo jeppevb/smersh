0fc99e795a97bdaf639f14ecddfbeec8a0ceb01c
Jeppe Vestergaard Boelsmand <jvb@netcompany.com>
2023-06-19 16:12:09 +0200

    adding endpoints and services

--- api/src/subjects/service.py
+++ api/src/subjects/service.py
@@ -1,1 +1,2 @@
-﻿
+﻿def get_subjects():
+    pass
--- api/src/subjects/views.py
+++ api/src/subjects/views.py
@@ -1,10 +1,19 @@
 ﻿import logging 
 from fastapi import APIRouter
 from .models import Subject, Vote
-
+from .schemas import Subject as DtoSubject, Vote as DtoVote
+from .service import get_subjects
 
 log = logging.getLogger(__name__)
 
 router = APIRouter()
 
+@router.get(
+    "",
+    response_model=List[Subject],
+    summary="Retrieves a list of subjects.",
+)
+def get_subjects(db_session: DbSession):
+    """Retrieves a list of subjects."""
+    return current_case
 
